02b3ee5d87e283d682635d414e7563dd0ea46614
AlexKarmaz <karmazct@mail.ru>
2017-03-23 13:21:11 +0300

    Add task2

---
+++ Task2.Tests/DoubleExtensionTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Task2.Tests
+{
+    [TestFixture]
+    public class DoubleExtensionTests
+    {
+        [TestCase(5.0 / 0 - 8.0 / 0, ExpectedResult = "1111111111111000000000000000000000000000000000000000000000000000")]
+        [TestCase(112.25, ExpectedResult = "0100000001011100000100000000000000000000000000000000000000000000")]
+        [TestCase(0, ExpectedResult = "0000000000000000000000000000000000000000000000000000000000000000")]
+        [TestCase(1, ExpectedResult = "0011111111110000000000000000000000000000000000000000000000000000")]
+        [TestCase(2, ExpectedResult = "0100000000000000000000000000000000000000000000000000000000000000")]
+        [TestCase(-112.25, ExpectedResult = "1100000001011100000100000000000000000000000000000000000000000000")]
+        [TestCase(0.1625, ExpectedResult = "0011111111000100110011001100110011001100110011001100110011001101")]
+        [TestCase(340.57, ExpectedResult = "0100000001110101010010010001111010111000010100011110101110000101")]
+        public static string ToBinaryRepresentation_NormalValuesAndSpecialValues_PositiveTest(double t)
+        {
+            return t.DoubleToBinary();
+        }
+    }
+}
---
+++ Task2/DoubleExtension.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    ///<summary>
+    /// Provides extension method for binary representation of
+    /// double-precision floating-point value
+    /// </summary>
+    public static class DoubleExtension
+    {
+        /// <summary>
+        /// Returns the binary representation of the number in string format.
+        /// </summary>
+        /// <param name="number">Source double-precision floating-point value</param>
+        /// <returns>The string with binary representation</returns>
+        public static string DoubleToBinary(this double number)
+        {
+            return BinaryRepresentation(number);
+        }
+
+        private static string BinaryRepresentation(double number)
+        {
+            var bits = BitConverter.DoubleToInt64Bits(number);
+
+            var sign = (bits >> 63) & 1;
+
+            var exponent = (bits >> 52) & (long)Math.Pow(2, 11) - 1;
+
+            var mantissa = (bits & (long)Math.Pow(2, 52) - 1);
+
+            return $"{Convert.ToString(sign, 2)}{Convert.ToString(exponent, 2).PadLeft(11, '0')}{Convert.ToString(mantissa, 2).PadLeft(52, '0')}"; 
+
+        }
+    }
+}
